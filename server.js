312215861c268490edff03607be421358793db1d
Julius Moyi <moyijulius17@gmail.com>
2025-03-29 11:44:57 -0700

    made changes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+const cors = require('cors');
+app.use(cors({ origin: 'https://crime-report-fronted.vercel.app/' }));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
